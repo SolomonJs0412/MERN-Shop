bdf7b0909c700f5d54ee6c7a48110a9fae1a216f
Tonight or never <vothuykg123@gmail.com>
2022-04-09 16:31:28 +0700

    header changed - show user in header - checked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { useEffect } from "react";
 
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
@@ -9,8 +10,15 @@ import ProductDetails from "./components/product/ProductDetails";
 
 import Login from "./components/user/Login";
 import Register from "./components/user/Register";
+import { loadUser } from "./actions/userActions";
+
+import store from "./store";
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Router>
       <div className="App">
