441d10503556b36b37b82aa77366f82d84fbc051
Tonight or never <vothuykg123@gmail.com>
2022-04-09 23:59:57 +0700

    update user logged in - checked

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import Login from "./components/user/Login";
 import Register from "./components/user/Register";
 import { loadUser } from "./actions/userActions";
 import Profile from "./components/user/Profile";
+import UpdateProfile from "./components/user/UpdateProfile";
 
 import ProtectedRoute from "./components/route/ProtectedRoute";
 
@@ -34,6 +35,7 @@ function App() {
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
           <ProtectedRoute path="/me" component={Profile} exact />
+          <ProtectedRoute path="/me/update" component={UpdateProfile} exact />
         </div>
         <Footer />
       </div>
