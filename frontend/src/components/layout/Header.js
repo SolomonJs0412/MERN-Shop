3a053aec3ccc6d020d4e984bf6a60de9f4806a45
Tonight or never <vothuykg123@gmail.com>
2022-04-09 20:13:40 +0700

    logout-checked

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -3,6 +3,7 @@ import { Route, Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useAlert } from "react-alert";
 import Search from "./Search";
+import { logout } from "../../actions/userActions";
 
 import "../../App.css";
 
@@ -11,6 +12,11 @@ const Header = () => {
   const dispatch = useDispatch();
 
   const { user, loading } = useSelector((state) => state.auth);
+
+  const logoutHandler = () => {
+    dispatch(logout());
+    alert.success("Đăng xuất thành công");
+  };
   return (
     <Fragment>
       <nav className="navbar row">
@@ -73,7 +79,11 @@ const Header = () => {
                 <Link className="dropdown-item" to="/me">
                   Thông tin
                 </Link>
-                <Link className="dropdown-item text-danger" to="/">
+                <Link
+                  className="dropdown-item text-danger"
+                  to="/"
+                  onClick={logoutHandler}
+                >
                   Đăng xuất
                 </Link>
               </div>
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -8,6 +8,8 @@ import {
   LOAD_USER_REQUEST,
   LOAD_USER_SUCCESS,
   LOAD_USER_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL,
   CLEAR_ERRORS,
 } from "../constants/userConstants.js";
 
@@ -29,12 +31,24 @@ export const authReducer = (state = { user: {} }, action) => {
         isAuthenticated: true,
         user: action.payload,
       };
+    case LOGOUT_SUCCESS:
+      return {
+        loading: false,
+        isAuthenticated: false,
+        user: null,
+      };
 
     case LOAD_USER_FAIL:
       return {
         loading: false,
         isAuthenticated: false,
         user: null,
+        error: action.payload,
+      };
+
+    case LOGOUT_FAIL:
+      return {
+        ...state,
         error: action.payload,
       };
 
